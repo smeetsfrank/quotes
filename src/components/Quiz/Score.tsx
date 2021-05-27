254cedbc06c7037463c266104d1e6ce2240d9d7b
smeetsfrank <franksmeets89@gmail.com>
2021-05-27 09:48:33 +0200

    cleanup Score component

--- src/components/Quiz/Score.tsx
+++ src/components/Quiz/Score.tsx
@@ -1,22 +1,22 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheck, faTimes } from '@fortawesome/free-solid-svg-icons';
-import { QuoteProps, GameProps } from '../../models/models';
+import { QuoteProps } from '../../models/models';
 
 import classes from './Score.module.scss';
 
 interface Props {
-    answers: any;
-    quotes: any;
+    answers: number[] | string[];
+    quotes: QuoteProps[];
 }
 
 const Score: React.FC<Props> = ({ answers, quotes }) => {
-  const checkedQuotes = quotes.map((quote: any, index: any) => (
+  const checkedQuotes = quotes.map((quote: QuoteProps, index) => (
     { ...quote, correct: quote.id.toString() === answers[index] }
   ));
 
   const scoreboardMessage = () => {
-    const numbOfCorrectAnwers = checkedQuotes.filter((quote: any) => quote.correct);
+    const numbOfCorrectAnwers = checkedQuotes.filter((quote: QuoteProps) => quote.correct);
     switch (numbOfCorrectAnwers.length) {
       case 0:
         return 'Zero, Seriously?! Did i just create a whole fucking game just for you to guess zero correct answers? IT EVEN IS MULTIPLE CHOICE!! The fuck is wrong with you?';
@@ -44,7 +44,7 @@ const Score: React.FC<Props> = ({ answers, quotes }) => {
         {scoreboardMessage()}
       </div>
       <ul>
-        {checkedQuotes && checkedQuotes.map((item: any) => (
+        {checkedQuotes && checkedQuotes.map((item: QuoteProps) => (
           <li key={item.id}>
             <h3>
               {item.author}
