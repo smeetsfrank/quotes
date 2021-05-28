cbc04c985a764286a835a8a6ee12fc924f75c075
smeetsfrank <franksmeets89@gmail.com>
2021-05-28 13:08:46 +0200

    option spec

---
+++ src/components/Quiz/Option.test.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Option from './Option';
+
+const optionComponent = (
+  <>
+    <Option
+      id={1}
+      author="Frank"
+      selectedOption={() => undefined}
+    />
+  </>
+);
+describe('Option', () => {
+  test('Renders correct label', () => {
+    render(optionComponent);
+    const label = screen.getByLabelText(/frank/i);
+    expect(label).toBeInTheDocument();
+  });
+
+  test('checks radio on click', () => {
+    render(optionComponent);
+    const label = screen.getByLabelText(/frank/i);
+    userEvent.click(label);
+    const radio = screen.getByRole('radio', { name: /frank/i });
+    expect(radio).toBeChecked();
+  });
+});
