e8074c803db4834b5b22cbb38ace9224a1100c24
smeetsfrank <franksmeets89@gmail.com>
2021-05-28 14:09:35 +0200

    update options spec

--- src/components/Quiz/Options.test.tsx
+++ src/components/Quiz/Options.test.tsx
@@ -2,17 +2,16 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Options from './Options';
 
-const authors = [
-  { id: 1, author: 'Frank' },
-  { id: 2, author: 'Anne' },
-];
-
-const answer = {
-  id: 3, author: 'Mik',
-};
-
 describe('Options', () => {
-  it('renders list of options', async () => {
+  const authors = [
+    { id: 1, author: 'Frank' },
+    { id: 2, author: 'Anne' },
+  ];
+
+  const answer = {
+    id: 3, author: 'Mik',
+  };
+  it('renders list of options with the correct label', async () => {
     const { getAllByRole } = render(
       <Options
         authors={authors}
@@ -22,5 +21,9 @@ describe('Options', () => {
     );
     const listItems = getAllByRole('listitem');
     expect(listItems).toHaveLength(3);
+
+    expect(screen.getByLabelText(/frank/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/anne/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/mik/i)).toBeInTheDocument();
   });
 });
